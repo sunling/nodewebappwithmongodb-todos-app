078b369ef8c96d89d9d688dd65a924c7cf347102
Ling Sun <lingsun@mum.edu>
2018-07-14 17:03:27 -0500

    set up heroku app

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
 
+//const mongolabUri = 'mongodb://heroku_dzrqmb9b:s6l218d0qq63vb8890155a97ns@ds137581.mlab.com:37581/heroku_dzrqmb9b';
 
+const localUri = 'mongodb://localhost:27017/TodoApp';
+mongoose.connect(process.env.MONGODB_URI || localUri);
 module.exports = {mongoose};
